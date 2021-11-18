1bb37fef8c893f7ceb362f4b447945b049bf1073
LyaminVS <vasya.lyamin03@gmail.com>
2021-11-18 17:27:48 +0300

    наполнил функцию main добавил главный цикл

--- main.py
+++ main.py
@@ -1,23 +1,169 @@
 import pygame
 import background
 import map_logic
-import menu
+# import menu
 import objects
 import sound
 import start_screen
 
 pygame.init()
-screen = pygame.display.set_mode(1280, 720)
-FPS = 30
-finished = False
-clock = pygame.time.Clock()
+
+
+class Params:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1280, 720))
+        """
+        Холст для рисования
+        """
+
+        self.FPS = 30
+        """
+        Количество кадров в секунду
+        """
+
+        self.finished = False
+        """
+        False если программа завешшена, иначе True
+        """
+
+        self.clock = pygame.time.Clock()
+        """
+        часы pygame
+        """
+
+        self.game_started = False
+        """
+        если была нажата кнопка старт на стартовом экране то True, иначе False
+        """
+
+        self.player_created = False
+        """
+        True если игрок создан, иначе False
+        """
+
+        self.player = None
+        """
+        пееменная содержащая объект игрока
+        """
+
+        self.all_objects = []
+        """
+        все объекты на карте
+        """
+
+
+def open_start_menu():
+    """
+    открывает стартовое меню игры
+    """
+    start_menu = start_screen.StartMenu(params.screen)
+    start_menu.draw()
+
+
+def line_to_object(line):
+    """
+    преобразовывает строку в объект класса Object
+    :param line: строка файла
+    :return:
+    """
+    pass
+
+
+def line_to_player(line):
+    """
+    преобразовывает строку в объект класса Player
+    :param line: строка файла
+    :return:
+    """
+    pass
+
+
+def get_player_from_file():
+    """
+    преобразовывает файл в объект класса Player
+    :return: объект класса Player
+    """
+    player_file = open("player_save.txt")
+    player = line_to_player(player_file)
+    player_file.close()
+    return player
+
+def get_obj_from_file():
+    """
+    преобразовывает файла в объект класса Object
+    :return: массив объектов класса Object
+    """
+    all_objects = []
+    objects_in_file = open("objects_save.txt")
+    for line in objects_in_file:
+        new_obj = line_to_object(line)
+        all_objects.append(new_obj)
+    objects_in_file.close()
+    return all_objects
+
+
+def create_start_position():
+    """
+    создается объект игрока, а также объекты зданий в первый кадр игры
+    :return:
+    """
+    params.player = line_to_player()
+    params.objects = get_obj_from_file()
+
+
+def update_game():
+    """
+    функция обновляет состояние игры
+    :return:
+    """
+    background.draw_map()
+    background.draw_objects()
+    map_logic.event_checker()
+
+
+def save_game():
+    """
+    функция сохраняет игру
+    :return:
+    """
+    player_file = open("player_save.txt", "w")
+    object_file = open("objects_save.txt", "w")
+    player_file.write(str(params.player) + "\n")
+    for obj in params.all_objects:
+        object_file.write(str(obj) + "\n")
+    player_file.close()
+    object_file.close()
+
+
+def game_quit():
+    """
+    функция обрабатывает закрытие окна игры
+    :return:
+    """
+    save_game()
+    pygame.quit()
+
+
+def main():
+    """
+    основной цикл программы
+    :return:
+    """
+    while not params.finished:
+        if not params.game_started:
+            open_start_menu()
+        else:
+            if not params.player_created:
+                create_start_position()
+            update_game()
+        params.clock.tick(params.FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                params.finished = True
+    game_quit()
 
 
 
-while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-
-pygame.quit()
+if __name__ == "__main__":
+    params = Params()
+    main()
