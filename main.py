668a58c9a3f113efdaed2d31ac02f2affcbaf6ad
LyaminVS <vasya.lyamin03@gmail.com>
2021-11-18 10:44:19 +0300

    исправил test_window и main

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import sound
 import start_screen
 
 pygame.init()
-
-
+screen = pygame.display.set_mode(1280, 720)
+FPS = 30
 finished = False
+clock = pygame.time.Clock()
+
 
 while not finished:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
