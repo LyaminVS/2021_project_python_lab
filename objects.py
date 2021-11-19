812d77c0177dc60532597ef36c4deb86e729fd4f
krssn <khanina.va@phystech.edu>
2021-11-19 17:48:24 +0300

    objects update

--- objects.py
+++ objects.py
@@ -1,15 +1,23 @@
 import pygame
-
+import os
 
 class Objects(pygame.sprite.Sprite):
     """
-    Класс свойств объекта с функцией отрисовки.
+    Класс объекта (пока только применим к зданиям).
     """
-
     def __init__(self, screen, image, name, x_0, y_0):
+        """
+        Конкструктор класса Objects.
+        :param screen: экран отрисовки
+        :param image: иллюстрация объекта
+        :param name: имя объекта
+        :param x_0: координата объекта по х
+        :param y_0: координата объекта по у
+        """
         super().__init__()
         self.name = name
         self.image = pygame.image.load(image)
+        self.image_name = os.path.abspath(image)
         self.surface = screen
         self.width = self.image.get_width()
         self.height = self.image.get_height()
@@ -18,23 +26,35 @@ class Objects(pygame.sprite.Sprite):
         self.inventory = None
 
     def draw(self, x, y):
+        """
+        Функция отрисовки объекта.
+        :param x: координата отрисовки по х (левая верхняя)
+        :param y: координата отрисовки по у (правая верхняя)
+        """
         self.surface.blit(self.y, (x, y))
 
     def __str__(self):
-        line = ";".join((self.name, self.image, self.x, self.y))
+        """
+        :return: параметры объекта одной строкой с разделителем ";"
+        """
+        line = ";".join((str(self.name), self.image_name, str(self.x, self.y))
         return line
 
 
 class Player(pygame.sprite.Sprite):
     """
-    инвентарь
-    скорость
-    различные состояния картинки
+    Класс игрока, наследующий Spite для проверки коллизий.
     """
-
     def __init__(self, screen, name, x_0, y_0):
+        """
+        Конструктор класса Player.
+        :param screen: экран отрисовки
+        :param name: имя объекта
+        :param x_0: координата игрока по оси х
+        :param y_0: координата игрока по оси у
+        """
         super().__init__()
-        self.name = name
+        self.name = "player"
         self.surface = screen
         self.width = self.image.get_width()
         self.height = self.image.get_height()
@@ -46,22 +66,34 @@ class Player(pygame.sprite.Sprite):
         self.inventory = None
 
     def draw(self, right):
+        """
+        Функция отрисовки игрока.
+        :param right: параметр, проверяющий, в какую сторону движется игрок
+        """
         if right == 1:
             self.image = pygame.image.load("skin run right1.png")
         else:
             self.image = pygame.image.load("skin run left1.png")
 
     def __str__(self):
+        """
+        :return: параметры объекта одной строкой с разделителем ";"
+        """
         line = ";".join((self.name, self.image, self.x, self.y))
         return line
 
 
 class Resources:
     """
-    от этого класса наследовать все ресурсы
+    Класс наследования всех ресурсов.
     """
-
     def __init__(self, screen, image, name):
+        """
+        Конструктор класса resources.
+        :param screen: поверхность отрисовки
+        :param image: иллюстрация ресурса
+        :param name: имя ресурса
+        """
         self.name = name
         self.image = pygame.image.load(image)
         self.surface = screen
@@ -70,6 +102,9 @@ class Resources:
         self.height = self.image.get_height()
 
     def __str__(self):
+        """
+        :return: параметры объекта одной строкой с разделителем ";"
+        """
         line = ";".join((self.name, self.image, self.width, self.height))
         return line
 
