28306bf8d33f9bad01da5db9e3387a5dbd77bbe7
krssn <khanina.va@phystech.edu>
2021-11-19 17:17:37 +0300

    objects update

--- objects.py
+++ objects.py
@@ -7,18 +7,23 @@ class Objects(pygame.sprite.Sprite):
     Класс свойств объекта с функцией отрисовки.
     """
 
-    def __init__(self, screen, image, x_0, y_0):
+    def __init__(self, screen, image, name, x_0, y_0):
         super().__init__()
+        self.name = name
         self.image = pygame.image.load(image)
         self.surface = screen
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.x = x_0
         self.y = y_0
-        self.menu = 0  # add
+        self.inventory = None
 
     def draw(self, x, y):
         self.surface.blit(self.y, (x, y))
+
+    def __str__(self):
+        line = str(self.name) + str(self.image) + str(self.x) + str(self.y)
+        return line
 
 
 class Player(pygame.sprite.Sprite):
@@ -28,8 +33,9 @@ class Player(pygame.sprite.Sprite):
     различные состояния картинки
     """
 
-    def __init__(self, screen, x_0, y_0):
+    def __init__(self, screen, name, x_0, y_0):
         super().__init__()
+        self.name = name
         self.surface = screen
         self.width = self.image.get_width()
         self.height = self.image.get_height()
@@ -38,6 +44,7 @@ class Player(pygame.sprite.Sprite):
         self.vx = 2
         self.vy = 2
         self.image = pygame.image.load("skin run right1.png")
+        self.inventory = None
 
     def draw(self, right):
         if right == 1:
@@ -45,17 +52,29 @@ class Player(pygame.sprite.Sprite):
         else:
             self.image = pygame.image.load("skin run left1.png")
 
+    def __str__(self):
+        line = str(self.name) + str(self.image) + str(self.x) + str(self.y)
+        return line
+
 
 class Resources:
     """
     от этого класса наследовать все ресурсы
     """
 
-    def __init__(self, screen, image):
+    def __init__(self, screen, image, name):
+        self.name = name
         self.image = pygame.image.load(image)
         self.surface = screen
+        self.amount = 15
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+    def __str__(self):
+        line = str(self.name) + str(self.image) + str(self.width) + str(self.height)
+        return line
 
 
 class Taco(Resources):
-    def __init__(self):
-        super(Shawarma, self).__init__()
+    def __init__(self, screen):
+        super(Taco, self).__init__(screen, "taco.png", "taco")
