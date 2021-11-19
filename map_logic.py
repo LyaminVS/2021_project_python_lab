f5436afdc7efda975d73f1a045680a25f48ee7e7
morgaiangelina <91495652+morgaiangelina@users.noreply.github.com>
2021-11-19 10:44:31 +0300

    Сделаны наброски функций event_checker() и player_move()

--- map_logic.py
+++ map_logic.py
@@ -1,8 +1,11 @@
+motion_keys_numbers = [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]
+
 def collision():
     """
     расчитывает коллизию
-    :return:
+    return:
     """
+    pass
 
 
 def player_toggle_inventory():
@@ -13,14 +16,43 @@ def player_toggle_object_inventory():
     pass
 
 
-def event_checker():
-    pass
-
-
-def player_move():
+def event_checker(event):
     """
-    смещает камеру и изменяет состояние игрока
-    :return:
+    Обработка нажатий мышью и на клавиатуру
+    Args:
+    event - событие на проверку
     """
-    pass
+    if event.type == pygame.KEYDOWN:
+        if event.key in motion_keys_numbers:
+            player_move(event)
+        elif event.key == pygame.K_e:
+            #функция для класса PlayerInventory()
+            pass
+        elif event.key == pygame.K_ESCAPE:
+            open_start_menu()
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        #взаимодействие с объектами
+        pass
 
+
+def player_move(motion_key_click_event):
+    """
+    пока что только передвигает все объекты
+    Args:
+    motion_key_click_event - обработка нажатий мыши и клавиатуры
+    return:
+    all_objects - массив всех объектов
+    """
+    if motion_key_click_event.key == pygame.K_w:
+        for obj in all_objects:
+            obj.y += player_speed*dt
+    elif motion_key_click_event.key==pygame.K_s:
+        for obj in all_objects:
+            obj.y -= player_speed*dt
+    elif motion_key_click_event.key==pygame.K_d:
+        for obj in all_objects:
+            obj.x -= player_speed*dt
+    elif motion_key_click_event.key==pygame.K_a:
+        for obj in all_objects:
+            obj.x += player_speed*dt
+    return all_objects
