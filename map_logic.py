144795963b41c93980558caf1f3293256ba5b384
morgaiangelina <91495652+morgaiangelina@users.noreply.github.com>
2021-11-24 20:56:39 +0300

    Удалены лишние аргументы

--- map_logic.py
+++ map_logic.py
@@ -3,32 +3,32 @@ import pygame
 motion_keys_numbers = [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]
 
 
-def collision(all_objects, player_class):
+def collision(params_class):
     """
     расчитывает коллизию
     """
-    for obj in all_objects():
-        if player_class.colliderect(obj.rect):
-            if ((player_class.y + player_class.height) > obj.y) and (player_class.y < obj.y):
-                player_class.y = obj.y - player_class.height
-            if ((player_class.y + player_class.height) > (obj.y + obj.height)) and (player_class.y > obj.y):
-                player_class.y = obj.y - player_class.height
-            if ((player_class.y + player_class.height) > obj.y) and (player_class.y < obj.y):
-                player_class.y = obj.y - player_class.height
-            if ((player_class.y + player_class.height) > (obj.y + obj.height)) and (player_class.y > obj.y):
-                player_class.y = obj.y - player_class.height
+    for obj in params_class.all_objects():
+        if params_class.player.colliderect(obj.rect):
+            if ((params_class.player.y + params_class.player.height) > obj.y) and (params_class.player.y < obj.y):
+                params_class.player.y = obj.y - params_class.player.height
+            if ((params_class.player.y + params_class.player.height) > (obj.y + obj.height)) and (params_class.player.y > obj.y):
+                params_class.player.y = obj.y - params_class.player.height
+            if ((params_class.player.y + params_class.player.height) > obj.y) and (params_class.player.y < obj.y):
+                params_class.player.y = obj.y - params_class.player.height
+            if ((params_class.player.y + params_class.player.height) > (obj.y + obj.height)) and (params_class.player.y > obj.y):
+                params_class.player.y = obj.y - params_class.player.height
 
 
 def event_checker(event_array, class_params):
     """
     Обработка нажатий мышью и на клавиатуру
     Args:
-    
+
     """
     for checked_event in event_array:
         if checked_event.type == pygame.KEYDOWN:
             if checked_event.key in motion_keys_numbers:
-                player_move(checked_event, player_class, params_array)
+                player_move(checked_event, class_params)
             elif checked_event.key == pygame.K_e:
                 # функция для класса PlayerInventory()
                 pass
@@ -36,25 +36,25 @@ def event_checker(event_array, class_params):
             pass
 
 
-def player_move(motion_key_click_event, player_class, params_array):
+def player_move(motion_key_click_event, params_class):
     """
     Изменяет координату объекта относительно экрана и студента относительно карты
     Args:
     motion_key_click_event - обработка нажатий мыши и клавиатуры
     """
     if motion_key_click_event.key == pygame.K_w:
-        player_class.y -= player_class.speed 
-        for obj in params_array.all_objects:
-            obj.y += player_class.speed
+        params_class.player.y -= params_class.player.speed
+        for obj in params_class.all_objects:
+            obj.y += params_class.player.speed
     elif motion_key_click_event.key == pygame.K_s:
-        player_class.y += player_class.speed
-        for obj in params_array.all_objects:
-            obj.y -= player_class.speed
+        params_class.player.y += params_class.player.speed
+        for obj in params_class.all_objects:
+            obj.y -= params_class.player.speed
     elif motion_key_click_event.key == pygame.K_d:
-        player_class.x += player_class.speed
-        for obj in params_array.all_objects:
-            obj.x -= player_class.speed
+        params_class.player.x += params_class.player.speed
+        for obj in params_class.all_objects:
+            obj.x -= params_class.player.speed
     elif motion_key_click_event.key == pygame.K_a:
-        player_class.x -= player_class.speed
-        for obj in params_array.all_objects:
-            obj.x += player_class.speed
+        params_class.player.x -= params_class.player.speed
+        for obj in params_class.all_objects:
+            obj.x += params_class.player.speed
