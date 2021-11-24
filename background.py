63864e82ee4ba3a66ba3e60391f458425acd899f
krssn <khanina.va@phystech.edu>
2021-11-24 19:14:15 +0300

    background change

--- background.py
+++ background.py
@@ -6,7 +6,7 @@ def draw_map(sc, x_0, y_0, objects):
     рисует карту
     :return:
     """
-    bg = pygame.image.load('background.png')
+    bg = pygame.image.load('pics/background.png')
     bg_rect = bg.get_rect()
     sc.blit(bg, (-x_0, -y_0))
     draw_objects(sc, objects, x_0, y_0)
