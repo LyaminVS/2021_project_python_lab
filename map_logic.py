73d988e6f60b82d1a22c9ef1fef6a7945c32fa74
morgaiangelina <91495652+morgaiangelina@users.noreply.github.com>
2021-12-01 19:47:51 +0300

    Теперь два кадра в анимации

--- map_logic.py
+++ map_logic.py
@@ -1,7 +1,5 @@
 import pygame
 
-motion_keys_numbers = [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]
-
 
 def event_checker(event_array, game):
     """
@@ -17,21 +15,40 @@ def event_checker(event_array, game):
             game.finished = True
         if checked_event.type == pygame.KEYUP:
             game.player.move = 0
-        if pygame.key.get_pressed() not in motion_keys_numbers:
-            game.player.up = 0
-            game.player.right = 0
         if pygame.key.get_pressed()[pygame.K_w]:
             game.player.vy = 2
+            if game.player.up == 1:
+                game.player.up = 2
+            else:
+                game.player.up = 1
         if pygame.key.get_pressed()[pygame.K_s]:
             game.player.vy = -2
+            if game.player.up == -1:
+                game.player.up = -2
+            else:
+                game.player.up = -1
         if pygame.key.get_pressed()[pygame.K_s] and pygame.key.get_pressed()[pygame.K_w]:
             game.player.vy = 0
+            game.player.up = 0
         if pygame.key.get_pressed()[pygame.K_d]:
             game.player.vx = 2
+            if game.player.right == 1:
+                game.player.right = 2
+            else:
+                game.player.right = 1
         if pygame.key.get_pressed()[pygame.K_a]:
             game.player.vx = -2
+            if game.player.right == -1:
+                game.player.right = -2
+            else:
+                game.player.right = -1
         if pygame.key.get_pressed()[pygame.K_d] and pygame.key.get_pressed()[pygame.K_a]:
             game.player.vx = 0
+            game.player.right = 0
+        if game.player.vx == 0:
+            game.player.right = 0
+        if game.player.vy == 0:
+            game.player.up = 0
         if pygame.key.get_pressed()[pygame.K_LSHIFT]:
             game.player.vx *= 2
             game.player.vy *= 2
@@ -53,6 +70,7 @@ def collision(game):
         if obj.collide_rect.colliderect(game.player.collide_rect):
             game.map[0] -= number_of_steps * game.player.vx
             game.player.vx = 0
+            game.player.right = 0
         else:
             game.map[0] -= number_of_steps * game.player.vx
         game.map[1] -= number_of_steps * game.player.vy
@@ -60,6 +78,7 @@ def collision(game):
         if obj.collide_rect.colliderect(game.player.collide_rect):
             game.map[1] += number_of_steps * game.player.vy
             game.player.vy = 0
+            game.player.up = 0
         else:
             game.map[1] += number_of_steps * game.player.vy
         game.map[0] += number_of_steps * game.player.vx
@@ -68,8 +87,10 @@ def collision(game):
         if obj.collide_rect.colliderect(game.player.collide_rect):
             game.map[0] -= number_of_steps * game.player.vx
             game.player.vx = 0
+            game.player.right = 0
             game.map[1] += number_of_steps * game.player.vy
             game.player.vy = 0
+            game.player.up = 0
         else:
             game.map[0] -= number_of_steps * game.player.vx
             game.map[1] += number_of_steps * game.player.vy
@@ -81,8 +102,6 @@ def player_move(game):
     Args:
     game - params из модуля main
     """
-    game.player.up = game.player.vy
-    game.player.right = game.player.vx
     game.map[0] += game.player.vx
     game.map[1] -= game.player.vy
     game.player.vx = 0
